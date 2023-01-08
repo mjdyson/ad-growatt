a00291f213b055a02797d8a87e350fbf43da32d6
mjdyson <mjdyson@gmail.com>
2023-01-08 21:37:40 +0000

    First test of working with device IDs as parameter

--- appdaemon/apps/growatt_app.py
+++ appdaemon/apps/growatt_app.py
@@ -8,10 +8,9 @@ class AD_Growatt(hass.Hass):
         self.listen_state(self.get_charge_settings, "input_button.adgw_get_charge_settings_button")
         self.listen_state(self.set_charge_settings, "input_button.adgw_set_charge_settings_button")
         self.listen_state(self.reset_time, "input_button.adgw_reset_sensors")
+        self.listen_state(self.get_device_ids, "input_button.adgw_get_device_ids_button")
 
-    def get_charge_settings(self, entity, attribute, old, new, kwargs):
-        #Args are pulled in from the apps.yaml file.
-        #It's good practice to have those values stored in the secrets file
+    def get_device_ids(self, entity, attribute, old, new, kwargs):
         un = self.args["growatt_username"]
         pwd = self.args["growatt_password"]
 
@@ -22,9 +21,42 @@ class AD_Growatt(hass.Hass):
         plant = plant_list['data'][0] #This is an array - we just take the first - would need a for-loop for more systems
         plant_id = plant['plantId']
         plant_info = api.plant_info(plant_id)
-        device = plant_info['deviceList'][0] #This is an array - we just take the first - would need a for-loop for more systems
-        device_sn = device['deviceSn']
 
+        self.entity_list = []
+
+        i = 0
+        while i < len(plant_info['deviceList']):
+            device = plant_info['deviceList'][i]
+            device_sn = device['deviceSn']
+            self.entity_list.append(device_sn)
+            i += 1
+
+        #device = plant_info['deviceList'][0] #This is an array - we just take the first - would need a for-loop for more systems
+        #device_sn = device['deviceSn']
+        self.call_service("input_select/set_options", entity_id="input_select.adgw_devices", options= self.entity_list)
+
+    def get_charge_settings(self, entity, attribute, old, new, kwargs):
+        #Args are pulled in from the apps.yaml file.
+        #It's good practice to have those values stored in the secrets file
+        un = self.args["growatt_username"]
+        pwd = self.args["growatt_password"]
+
+        api = growattServer.GrowattApi(True) #get an instance of the api, using a random string as the ID
+        session = api.login(un, pwd) #login and return a session
+
+        #Device ID from the input_select
+        device_sn = self.get_state("input_select.adgw_devices")
+
+        #If device_sn has not been created, get the first device in the plant
+        #I dont like that this is repeated code, it works though will refactor later
+        if device_sn == "---":
+            plant_list = api.plant_list(session['user']['id'])
+            plant = plant_list['data'][0] #This is an array - we just take the first - would need a for-loop for more systems
+            plant_id = plant['plantId']
+            plant_info = api.plant_info(plant_id)
+            device = plant_info['deviceList'][0]
+            device_sn = device['deviceSn']
+        
         #Query the server using the api
         response = api.get_mix_inverter_settings(device_sn)
 
@@ -42,12 +74,18 @@ class AD_Growatt(hass.Hass):
         api = growattServer.GrowattApi(True) #get an instance of the api
         session = api.login(un, pwd) #login and return a session
 
-        plant_list = api.plant_list(session['user']['id'])
-        plant = plant_list['data'][0] #This is an array - we just take the first - would need a for-loop for more systems
-        plant_id = plant['plantId']
-        plant_info = api.plant_info(plant_id)
-        device = plant_info['deviceList'][0] #This is an array - we just take the first - would need a for-loop for more systems
-        device_sn = device['deviceSn']
+        #Device ID from the input_select
+        device_sn = self.get_state("input_select.adgw_devices")
+
+        #If device_sn has not been created, get the first device in the plant
+        #I dont like that this is repeated code, it works though will refactor later
+        if device_sn == "---":
+            plant_list = api.plant_list(session['user']['id'])
+            plant = plant_list['data'][0] #This is an array - we just take the first - would need a for-loop for more systems
+            plant_id = plant['plantId']
+            plant_info = api.plant_info(plant_id)
+            device = plant_info['deviceList'][0]
+            device_sn = device['deviceSn']
 
         # Read the values required from HA and put them into variables
         start_timestamp = self.parse_time(self.get_state("input_datetime.adgw_battery_first_time_slot_1_start"))
