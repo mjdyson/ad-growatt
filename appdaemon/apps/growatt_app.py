dd85bfb5a74807472b41105ae0eaf94733d8947a
mjdyson <mjdyson@gmail.com>
2023-01-28 07:34:03 +0000

    Refactored to remove duplicated code

--- appdaemon/apps/growatt_app.py
+++ appdaemon/apps/growatt_app.py
@@ -1,6 +1,7 @@
 import hassapi as hass
 import growattServer
 import time
+import datetime
 
 class AD_Growatt(hass.Hass):
 
@@ -9,53 +10,42 @@ class AD_Growatt(hass.Hass):
         self.listen_state(self.set_charge_settings, "input_button.adgw_set_charge_settings_button")
         self.listen_state(self.reset_time, "input_button.adgw_reset_sensors")
         self.listen_state(self.get_device_ids, "input_button.adgw_get_device_ids_button")
+        self.un = self.args["growatt_username"]
+        self.pwd = self.args["growatt_password"]
+        self.api = growattServer.GrowattApi(True) #get an instance of the api, using a random string as the ID
 
     def get_device_ids(self, entity, attribute, old, new, kwargs):
-        un = self.args["growatt_username"]
-        pwd = self.args["growatt_password"]
 
-        api = growattServer.GrowattApi(True) #get an instance of the api, using a random string as the ID
-        session = api.login(un, pwd) #login and return a session
-        plant_list = api.plant_list(session['user']['id'])
+        session = self.api.login(self.un, self.pwd) #login and return a session
+        plant_list = self.api.plant_list(session['user']['id'])
 
         self.entity_list = []
         for plant in plant_list['data']:
             # For all the plants on the account
             plant_id = plant['plantId']
-            plant_info = api.plant_info(plant_id)
+            plant_info = self.api.plant_info(plant_id)
 
             for device in plant_info['deviceList']:
                 # For all the devices in the plant, add them to the entity_list
                 device_sn = device['deviceSn']
                 self.entity_list.append(device_sn)
 
-        self.call_service("input_select/set_options", entity_id="input_select.adgw_devices", options= self.entity_list)
-        self.call_service("input_select/select_first", entity_id="input_select.adgw_devices")
+        self.call_service("input_select/set_options", entity_id="input_select.adgw_devices", options=self.entity_list)
+        self.call_service("input_select/select_last", entity_id="input_select.adgw_devices")
 
     def get_charge_settings(self, entity, attribute, old, new, kwargs):
-        #Args are pulled in from the apps.yaml file.
-        #It's good practice to have those values stored in the secrets file
-        un = self.args["growatt_username"]
-        pwd = self.args["growatt_password"]
 
-        api = growattServer.GrowattApi(True) #get an instance of the api, using a random string as the ID
-        session = api.login(un, pwd) #login and return a session
+        session = self.api.login(self.un, self.pwd) #login and return a session
 
         #Device ID from the input_select
         device_sn = self.get_state("input_select.adgw_devices")
 
         #If device_sn has not been created, get the first device in the plant
-        #I dont like that this is repeated code, it works though will refactor later
         if device_sn == "---":
-            plant_list = api.plant_list(session['user']['id'])
-            plant = plant_list['data'][0] #This is an array - we just take the first - would need a for-loop for more systems
-            plant_id = plant['plantId']
-            plant_info = api.plant_info(plant_id)
-            device = plant_info['deviceList'][0]
-            device_sn = device['deviceSn']
-        
+            device_sn = self.get_first_device()
+
         #Query the server using the api
-        response = api.get_mix_inverter_settings(device_sn)
+        response = self.api.get_mix_inverter_settings(device_sn)
 
         #Create new sensors in HA and populate them with the values we've just received from the server.
         self.set_state("sensor.template_adgw_battery_first_time_slot_1_start", state = response['obj']['mixBean']['forcedChargeTimeStart1'])
@@ -65,24 +55,15 @@ class AD_Growatt(hass.Hass):
         self.set_state("sensor.template_adgw_battery_first_ac_charge_enabled", state = response['obj']['mixBean']['acChargeEnable'])
 
     def set_charge_settings(self, entity, attribute, old, new, kwargs):
-        un = self.args["growatt_username"]
-        pwd = self.args["growatt_password"]
 
-        api = growattServer.GrowattApi(True) #get an instance of the api
-        session = api.login(un, pwd) #login and return a session
+        session = self.api.login(self.un, self.pwd) #login and return a session
 
         #Device ID from the input_select
         device_sn = self.get_state("input_select.adgw_devices")
 
         #If device_sn has not been created, get the first device in the plant
-        #I dont like that this is repeated code, it works though will refactor later
         if device_sn == "---":
-            plant_list = api.plant_list(session['user']['id'])
-            plant = plant_list['data'][0] #This is an array - we just take the first - would need a for-loop for more systems
-            plant_id = plant['plantId']
-            plant_info = api.plant_info(plant_id)
-            device = plant_info['deviceList'][0]
-            device_sn = device['deviceSn']
+            device_sn = self.get_first_device()
 
         # Read the values required from HA and put them into variables
         start_timestamp = self.parse_time(self.get_state("input_datetime.adgw_battery_first_time_slot_1_start"))
@@ -92,8 +73,6 @@ class AD_Growatt(hass.Hass):
         # Convert on/off to 1/0
         ac_charge_on = convert_on_off(self.get_state("input_boolean.adgw_ac_charge_on"))
         battery_first_time_slot_1_enabled = convert_on_off(self.get_state("input_boolean.adgw_battery_first_time_slot_1_enabled"))
-        #self.log(battery_first_time_slot_1_enabled)
-        #self.log(ac_charge_on)
 
         # Create dictionary of settings to apply through the api call. The order of these elements is important.
         schedule_settings = ["100", #Charging power %
@@ -110,7 +89,7 @@ class AD_Growatt(hass.Hass):
                                 "0"]        #Schedule 3 - Enabled/Disabled (0 = Disabled)
 
         # The api call - specifically for the mix inverter. Some other op will need to be applied if you dont have a mix inverter (replace 'mix_ac_charge_time_period')
-        response = api.update_mix_inverter_setting(device_sn, 'mix_ac_charge_time_period', schedule_settings)
+        response = self.api.update_mix_inverter_setting(device_sn, 'mix_ac_charge_time_period', schedule_settings)
 
     def reset_time(self, entity, attribute, old, new, kwargs):
         #This section is just for testing and can be deleted if required. 
@@ -122,9 +101,19 @@ class AD_Growatt(hass.Hass):
         self.set_state("sensor.template_adgw_battery_first_ac_charge_enabled", state = "0")
         self.set_state("sensor.template_adgw_battery_first_time_slot_1_enabled", state = "0")
 
+    def get_first_device():
+        plant_list = self.api.plant_list(session['user']['id'])
+        plant = plant_list['data'][0] #This is an array - we just take the first - would need a for-loop for more systems
+        plant_id = plant['plantId']
+        plant_info = self.api.plant_info(plant_id)
+        device = plant_info['deviceList'][0]
+        device_sn = device['deviceSn']
+        return device_sn
+
 def convert_on_off(value):
     # Function to convert on/off to 1/0
     if value == "on":
         return "1"
     else:
         return "0"
+
